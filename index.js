5cb1e3587c078e36e7f7eae12c8b010a64fb7a20
Evgeniy Ryazancev <ryazancev.e@gmail.com>
2021-10-19 23:45:25 +0300

    lk

--- index.js
+++ index.js
@@ -30,3 +30,38 @@ const addFavorite = () => {
     books.addEventListener('click', fillHeart)
 };
 addFavorite();
+
+const showModalLk = () => {
+    const modalContainer = document.querySelector('.modal');
+    const modalLk = document.querySelector('.lk-modal');
+    const lkButtons = document.querySelectorAll('.js-lk');
+
+    const openModal = (e) => {
+        e.preventDefault();
+        modalContainer.classList.add('open');
+        modalLk.classList.add('open');
+    };
+
+    const closeModal = () => {
+        modalContainer.classList.remove('open');
+        for (let modal of modalContainer.children) {
+            modal.classList.remove('open');
+        }
+    };
+
+    lkButtons.forEach(btn => {
+        btn.addEventListener('click', openModal);
+    });
+
+    modalContainer.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target.closest('.modal__close')) {
+            closeModal();
+        }
+        if (target.matches('.modal')) {
+            closeModal();
+        }
+    });
+
+};
+showModalLk();
